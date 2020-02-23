04f74678be0c105452a8333e2d8efb7629ea3d72
Roman Janovsky <roman.janovsky9@seznam.cz>
2020-02-23 20:00:15 +0100

    added dragons

--- app/src/main/java/csm/czech_soil_map/ui/gallery/GalleryFragment.java
+++ app/src/main/java/csm/czech_soil_map/ui/gallery/GalleryFragment.java
@@ -12,6 +12,8 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import org.w3c.dom.Text;
+
 import csm.czech_soil_map.R;
 
 public class GalleryFragment extends Fragment {
@@ -23,6 +25,7 @@ public class GalleryFragment extends Fragment {
         galleryViewModel =
                 ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +33,15 @@ public class GalleryFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        final TextView dragons = root.findViewById(R.id.text_dragons);
+        galleryViewModel.getDragons().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                dragons.setText(s);
+            }
+        });
+
         return root;
     }
 }
--- app/src/main/java/csm/czech_soil_map/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/csm/czech_soil_map/ui/gallery/GalleryViewModel.java
@@ -7,13 +7,21 @@ import androidx.lifecycle.ViewModel;
 public class GalleryViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
+    private MutableLiveData<String> mDragons;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is gallery fragment");
+
+        mDragons  = new MutableLiveData<>();
+        mDragons.setValue("Beware...\n\n\nHere be dragons");
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+
+    public LiveData<String> getDragons() {
+        return mDragons;
     }
 }
