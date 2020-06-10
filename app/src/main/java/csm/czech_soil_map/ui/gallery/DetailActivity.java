cc9f5de7a24850dc6bf263117894f810d8038bc5
Barilac <janovskym@email.cz>
2020-06-10 19:01:34 +0200

    Added customiyation of soil list, cache tile map.

--- app/src/main/java/csm/czech_soil_map/ui/gallery/DetailActivity.java
+++ app/src/main/java/csm/czech_soil_map/ui/gallery/DetailActivity.java
@@ -1,10 +1,11 @@
 package csm.czech_soil_map.ui.gallery;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import csm.czech_soil_map.R;
 
@@ -16,7 +17,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
         setTitle(intent.getStringExtra("soil_name"));
-        ((TextView)findViewById(R.id.soil_name_detail_id)).setText(intent.getStringExtra("soil_name"));
+        ImageView iv = null;
         ((TextView)findViewById(R.id.soil_text_detail_id)).setText(intent.getStringExtra("soil_text"));
     }
 }
